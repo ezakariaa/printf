d752632af9a05193fac588e9dbcb60d5df3342d5
Badrdineyk <badrdineyk9@gmail.com>
2023-08-21 13:06:19 -0700

    Add: main.h file

--- main.h
+++ main.h
@@ -19,22 +19,22 @@ typedef struct format
 	int (*func)(va_list);
 } cf_t;
 
-void print_Hexadecimal(unsigned int number, int *ptr);
-int print_ex_string(va_list arg);
-int print_hexadecimal_pointer(va_list arg);
-int print_address(va_list arg);
-int print_binary(va_list arg);
-int print_unsigned(va_list arg);
-int print_octal(va_list arg);
-int print_hexadecimal(va_list arg);
-int print_Hexadecimal(va_list arg);
-int reverse_string(va_list arg);
-int length_integer(int arg);
 int _printf(const char *format, ...);
 int _putchar(char character);
+void print_Hexa(unsigned int number, int *ptr);
+int length_integer(int arg);
+int print_address(va_list arg);
+int print_hexadecimal_pointer(va_list arg);
+int print_binary(va_list arg);
 int print_character(va_list arg);
-int print_modulo(va_list arg);
+int print_ex_string(va_list arg);
+int print_Hexadecimal(va_list arg);
+int print_hexadecimal(va_list arg);
 int print_integer(va_list arg);
+int print_modulo(va_list arg);
+int print_octal(va_list arg);
 int print_string(va_list arg);
+int print_unsigned(va_list arg);
+int reverse_string(va_list arg);
 
 #endif
