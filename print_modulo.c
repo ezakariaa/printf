8792272eae3cfc2cbb2eec6aa4d6ff79594e6244
ezakariaa <zakariaorche@gmail.com>
2023-08-22 11:34:28 -0700

    ADDING print_modulo.c

---
+++ print_modulo.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_modulo - Prints the modulo (%) of a number
+ * @arg: A pointer to the first arg
+ *
+ * Return: always 1.
+ */
+int print_modulo(va_list arg)
+{
+	(void)arg;
+	_putchar('%');
+
+	return (1);
+}
