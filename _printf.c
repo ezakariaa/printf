08c2051fcd79fa854c7f77fd10dc286b28201018
Badrdineyk <badrdineyk9@gmail.com>
2023-08-22 03:42:41 -0700

    Add: _printf.c file

--- _printf.c
+++ _printf.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
-int h_hashtag(char char, int *ptr);
-int h_space_plus(char char, int number);
+int h_hashtag(char character, int *ptr);
+int h_space_plus(char character, int number);
 int _printf(const char *format, ...);
 
 /**
  * h_hashtag - handle the hashtag (#) sign.
- * @char: The character after the hashtag sign.
+ * @charater: The character after the hashtag sign.
  * @ptr: pointer to current character.
  *
  * Return: The length of the printed character.
  */
-int h_hashtag(char char, int *ptr)
+int h_hashtag(char character, int *ptr)
 {
 	int length = 0;
 
-	if (char == 'x' || char == 'X')
+	if (character == 'x' || character == 'X')
 	{
 		_putchar('0');
 		_putchar('x');
 		length += 2;
 		(*ptr)++;
 	}
-	else if (char == 'o')
+	else if (character == 'o')
 	{
 		_putchar('0');
 		length++;
@@ -33,16 +33,16 @@ int h_hashtag(char char, int *ptr)
 
 /**
  * h_space_plus - handle spaces and plus (+) sign after the modulo (%).
- * @char: The chararacter modulo (%) sign.
+ * @character: The chararacter modulo (%) sign.
  * @number: Printed number
  *
  * Return: The length of the printed character.
  */
-int h_space_plus(char char, int number)
+int h_space_plus(char character, int number)
 {
 	int length = 0;
 
-	if (char == '+')
+	if (character == '+')
 	{
 		int _is_negative;
 
@@ -75,9 +75,9 @@ int _printf(const char *format, ...)
 	int j;
 	va_list arg;
 
-	cf_t functions[] = {{'c', print_char}, {'s', print_string},
+	cf_t functions[] = {{'c', print_character}, {'s', print_string},
 		{'%', print_modulo}, {'i', print_integer},
-		{'d', print_integer}, {'r', reverse_str},
+		{'d', print_integer}, {'r', reverse_string},
 		{'x', print_hexadecimal}, {'X', print_Hexadecimal},
 		{'o', print_octal}, {'u', print_unsigned},
 		{'b', print_binary}, {'p', print_address},
@@ -99,7 +99,7 @@ int _printf(const char *format, ...)
 
 				va_copy(ap, arg);
 				length += h_space_plus(format[index + 1], va_arg(ap, int));
-				i++;
+				index++;
 			}
 			else if (format[index + 1] == '#')
 			{
